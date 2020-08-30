ea7d7b86f2d30cce90e6846c8a2e9e4c7ba381d5
Raymond <raymond13gggg@gmail.com>
2020-08-30 09:39:40 -0700

    more comments

--- Lab-1/ModifiedInsertionSort.java
+++ Lab-1/ModifiedInsertionSort.java
@@ -12,13 +12,13 @@ public class ModifiedInsertionSort {
     public static String[] insertionSort(String[] args){
         for (int j = 1; j < args.length; j++){ //begininng loop to get 2nd element of array
             String key = args[j];
-            String stringj = key.substring(0, key.length() - 1);
+            String stringj = key.substring(0, key.length() - 1); // comparing all characters except the last character of string
             int i = j - 1;
             int numberOfComparisons = 0;
             if ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){ //count the instance in which element compares previous element
                 numberOfComparisons++;                                                  // but does not go into loop
             }
-            while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){ //comparison of previous element
+            while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){ //comparison of previous element(s)
                 args[i + 1] = args[i];
                 i = i -1;
                 numberOfComparisons++;
