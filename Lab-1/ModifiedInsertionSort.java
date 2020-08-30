3755bdfeec451fe7fe0144c258216e4f4efa1499
Raymond <raymond13gggg@gmail.com>
2020-08-30 09:21:02 -0700

    commenting code

--- Lab-1/ModifiedInsertionSort.java
+++ Lab-1/ModifiedInsertionSort.java
@@ -10,20 +10,20 @@ public class ModifiedInsertionSort {
     }
 
     public static String[] insertionSort(String[] args){
-        for (int j = 1; j < args.length; j++){
+        for (int j = 1; j < args.length; j++){ //begininng loop to get 2nd element of array
             String key = args[j];
             String stringj = key.substring(0, key.length() - 1);
             int i = j - 1;
             int numberOfComparisons = 0;
-            if ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){
-                numberOfComparisons++;
+            if ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){ //count the instance in which element compares previous element
+                numberOfComparisons++;                                                  // but does not go into loop
             }
-            while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){
+            while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){ //comparison of previous element
                 args[i + 1] = args[i];
                 i = i -1;
                 numberOfComparisons++;
-                if ((i >= 0 && args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){
-                    numberOfComparisons++;
+                if ((i >= 0 && args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){ //Compare the instance in which element compares previous element
+                    numberOfComparisons++;                                                          // but does not return back into loop to count comparison
                 }
             }
             System.out.println(key + " "  + numberOfComparisons);
