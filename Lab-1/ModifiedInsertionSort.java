5b6b24f3ca5a863fe45b346ee58f234c23dfdee8
Raymond <raymond13gggg@gmail.com>
2020-08-29 22:11:56 -0700

    updated program

--- Lab-1/ModifiedInsertionSort.java
+++ Lab-1/ModifiedInsertionSort.java
@@ -5,10 +5,10 @@
 import java.util.Arrays;
 public class ModifiedInsertionSort {
     public static void main(String[] args) {
-        
-        String s = "raymond";
-        String r = "ramond";
-        System.out.println(s.compareTo(r));
+
+
+     String[] sortedList = insertionSort(args);
+     System.out.println(Arrays.toString(sortedList));
     
 
     }
@@ -16,16 +16,21 @@ public class ModifiedInsertionSort {
     public static String[] insertionSort(String[] args){
         for (int j = 1; j < args.length; j++){
             String key = args[j];
-            String stringj = args[j].substring(0, key.length() - 1);
+            String stringj = key.substring(0, key.length() - 1);
             int i = j - 1;
-            String stringi = args[i].substring(0, args[i].length() - 1);
             int numberOfComparisons = 0;
-            while (i >= 0 && (stringi.compareTo(stringj) > 0)){
+            if ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){
+                numberOfComparisons++;
+            }
+            while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){
                 args[i + 1] = args[i];
                 i = i -1;
                 numberOfComparisons++;
+                if ((i == 0 && args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){
+                    numberOfComparisons++;
+                }
             }
-            System.out.println(args[j] + " "  + numberOfComparisons);
+            System.out.println(key + " "  + numberOfComparisons);
             args[i+1] = key;
         }
         return args;
