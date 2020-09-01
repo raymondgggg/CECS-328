ec3710b8c70dbd441deb24a0239c0cf41da1cbe9
Raymond <raymond13gggg@gmail.com>
2020-08-31 18:16:50 -0700

    final

--- Lab-1/ModifiedInsertionSort.java
+++ Lab-1/ModifiedInsertionSort.java
@@ -2,7 +2,6 @@
 // Brent Nishioka
 // Scott Sakurai
 // CECS 328-Sec09
-import java.util.*;
 public class ModifiedInsertionSort {
     public static void main(String[] args) {
         insertionSort(args);
@@ -10,11 +9,8 @@ public class ModifiedInsertionSort {
     }
 
     public static void insertionSort(String[] args){
-        HashMap<String, Integer> comparisons = new HashMap<String, Integer>();
-        int numberOfInitialComparisons = 0;
-        for (int i =0; i < args.length; i++){ //populate the hasmap with the keys and the initial comparisons
-            comparisons.put(args[i], numberOfInitialComparisons);
-        }
+        String[] originalArgs = args.clone();
+        int[] comparisons = new int[args.length];
 
         for (int j = 1; j < args.length; j++){ //begininng loop to get 2nd element of array
             String key = args[j];
@@ -23,28 +19,31 @@ public class ModifiedInsertionSort {
             int numberOfComparisons = 0;
 
             if ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){ //count the instance in which element compares previous element
-                numberOfComparisons++;
-                comparisons.replace(key,numberOfComparisons);                                                 // but does not go into loop
+                numberOfComparisons++;                                                 // but does not go into loop to update the count
+                comparisons[j] = numberOfComparisons;                                                 
             }
 
             while (i >= 0 && ((args[i].substring(0, args[i].length() - 1).compareTo(stringj) > 0))){ //comparison of previous element(s)
                 args[i + 1] = args[i];
                 i = i -1;
                 numberOfComparisons++;
-                comparisons.replace(key, numberOfComparisons);
+                comparisons[j] = numberOfComparisons;
                 if ((i >= 0 && args[i].substring(0, args[i].length() - 1).compareTo(stringj) <= 0)){ //Compare the instance in which element compares previous element
-                    numberOfComparisons++;  
-                    comparisons.replace(key, numberOfComparisons);                                                        // but does not return back into loop to count comparison
+                    numberOfComparisons++;                                                           // but does not return back into loop to update count comparison
+                    comparisons[j] = numberOfComparisons;                                                     
                 }
             }
             args[i+1] = key;
         }
 
-        for (int i = 0; i < args.length; i++){
+        for (int i = 0; i < args.length; i++){ // printing sorted list 
             System.out.print(args[i] + " ");
         }
+
         System.out.println();
-        comparisons.forEach((k,v) -> System.out.println(k + " " + v));
-        
+
+        for (int i = 1; i < originalArgs.length; i ++){ // printing the number of comparisons in the order of original input
+            System.out.println(originalArgs[i] + " " + comparisons[i]);
+        }
     }
 }
