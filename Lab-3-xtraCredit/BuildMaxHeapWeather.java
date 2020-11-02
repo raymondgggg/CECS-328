654dec6c1891f8dc3dbe21c9ba9f3588a90b39a7
Raymond <raymond13gggg@gmail.com>
2020-11-02 13:17:47 -0800

    updates

--- Lab-3-xtraCredit/BuildMaxHeapWeather.java
+++ Lab-3-xtraCredit/BuildMaxHeapWeather.java
@@ -1,13 +1,72 @@
-// Scott Su
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class BuildMaxHeapWeather{
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class BuildMaxHeapWeather {
+
     public static void main(String[] args) {
-        
+        int temp = (int) Math.floor(makeRequest(args[0]));
+        int[] maxHeapArray = new int[temp];
+        System.out.println(temp + " randomly generated integers:");
+        for (int i = 0; i < temp; i++){
+            int j = ThreadLocalRandom.current().nextInt(201);
+            maxHeapArray[i] = j;
+            System.out.print(j + " ");
+        }
+        System.out.println("\n\nMax Heap Array:");
+        buildMaxHeap(maxHeapArray);
+        printMaxHeap(maxHeapArray);
     }
-    
+
+    public static double makeRequest(String location) {
+        String key = "5af53fae335b496725f258a89fff5286";
+        String urlStr = "http://api.openweathermap.org/data/2.5/weather?appid=" + key + "&q=" + location
+                + "&units=imperial";
+        double temp = 0;
+        try {
+            StringBuilder result = new StringBuilder();
+            URL url = new URL(urlStr);
+            URLConnection connection = url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            reader.close();
+            String jsonStr = result.toString();
+            temp = getTemp(jsonStr);
+          
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return temp;
+    }
+
+    public static double getTemp(String jsonStr){
+        double temp = 0;
+        JSONParser p = new JSONParser();
+        try {
+            var data = p.parse(jsonStr);
+            JSONObject jObject = (JSONObject) data;
+            JSONObject mainData = (JSONObject) jObject.get("main");
+            temp = (double) mainData.get("temp");
+            return temp;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
     /**
      * Build max heap subroutine where the elements of array are inserted into the
      * heap
+     * 
      * @param A the array
      * @param i the index
      */
@@ -33,7 +92,7 @@ public class BuildMaxHeapWeather{
 
     /**
      * Method where the max heap is made
-     * 
+     *
      * @param A the array
      */
     public static void buildMaxHeap(int[] A) {
@@ -44,7 +103,7 @@ public class BuildMaxHeapWeather{
 
     /**
      * get the left child of index
-     * 
+     *
      * @param i index inputted
      * @return left child location index
      */
@@ -54,7 +113,7 @@ public class BuildMaxHeapWeather{
 
     /**
      * Get the right child of index
-     * 
+     *
      * @param i index inputted
      * @return right child location index
      */
@@ -64,7 +123,7 @@ public class BuildMaxHeapWeather{
 
     /**
      * return the height of the heap
-     * 
+     *
      * @param aLen length of array
      * @return the height
      */
@@ -74,6 +133,7 @@ public class BuildMaxHeapWeather{
 
     /**
      * Print the max heap
+     * 
      * @param A the array representation of max heap
      */
     public static void printMaxHeap(int[] A) {
